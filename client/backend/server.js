c68b1adb5f2731a19ecc6c155af98b52d5eced8b
john-git7 <gideonebenezer467@gmail.com>
2025-05-16 04:22:04 +0530

    user/pass authenticated

---
+++ client/backend/server.js
@@ -0,0 +1,39 @@
+
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const authRoutes = require('./routes/authRoutes');  
+const musicRoutes = require('./routes/musicRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/api/auth', authRoutes);
+app.use('/api/search', searchRoutes);
+app.use('/api/audio', musicRoutes);
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('MongoDB connection failed:', err.message);
+  });
